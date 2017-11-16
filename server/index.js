5e784e3e709657a8f6625b18949e580c3c750ef8
dwrz <dwrz@dwrz.net>
2017-11-16 23:05:33 +0000

    Setup basic routing endpoints

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello, World.');
+});
+
+app.get('/login', (req, res) => {
+  
+});
+
+app.get('/signup', (req, res) => {
+
+});
+
+
+app.get('/username', (req, res) => {
+
+});
+
+
+app.get('/api/username/event', (req, res) => {
+
+});
+
+app.post('/api/username/logEvent', (req, res) => {
+
+});
+
+app.post('/api/username/createEventType', (req, res) => {
+
 });
 
 app.listen(PORT, () => {
