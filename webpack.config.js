f29c3d59289476126bea7108da69bc1be5501849
dwrz <dwrz@dwrz.net>
2017-11-16 23:35:17 +0000

    Attempt to fix webpack issues caused by eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ const BUILD_DIR = __dirname + '/client/public'; //path //dirname/src/public bund
 const APP_DIR = __dirname + '/client/src'; // index.jsx directory (react file)
 
 module.exports = {
-	entry: `${APP_DIR}/index.jsx`, // FILE WE WILL BUNDLE FROM
-	output: {
-		path: BUILD_DIR, // WHERE BUNDLE.JS WILL GO
-		filename: 'bundle.js', // TRANSPILED FILE
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?/, // Specifying file type that will be transpiled
-			include: APP_DIR,
-			loader: 'babel-loader', // Tells which transpiler to use
-			query: {
-				presets: ['react', 'es2015'], // This could also be put in a .babelrc file
-			},
-		},
-		],
-	},
+  entry: `${APP_DIR}/index.jsx`, // FILE WE WILL BUNDLE FROM
+  output: {
+    path: BUILD_DIR, // WHERE BUNDLE.JS WILL GO
+    filename: 'bundle.js' // TRANSPILED FILE
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/, // Specifying file type that will be transpiled
+      include: APP_DIR,
+      loader: 'babel-loader', // Tells which transpiler to use
+      query: {
+	presets: ['react', 'es2015'] // This could also be put in a .babelrc file
+      }
+    }
+   ]
+  }
 };
