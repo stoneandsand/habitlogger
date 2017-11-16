27a4c64735f3451da6eff63c4ec9f66265300a1b
dwrz <dwrz@dwrz.net>
2017-11-16 03:48:57 +0000

    Fixes for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,20 @@ const BUILD_DIR; //path //dirname/src/public bundle.js directory
 const APP_DIR; // index.jsx directory (react file)
 
 module.exports = {
-  entry: APP_DIR + '/index.jsx', //FILE WE WILL BUNDLE FROM
+  entry: APP_DIR + '/index.jsx', // FILE WE WILL BUNDLE FROM
   output: {
-    path: BUILD_DIR, //WHERE BUNDLE.JS WILL GO
-    filename: 'bundle.js' //TRANSPILED FILE
+    path: BUILD_DIR, // WHERE BUNDLE.JS WILL GO
+    filename: 'bundle.js' // TRANSPILED FILE
   },
   module: {
-    loaders: [
-      test: /\.jsx?/, //specifying file type that will be transpiled
+    loaders: [{
+      test: /\.jsx?/, // Specifying file type that will be transpiled
       include: APP_DIR,
-      loader: 'babel-loader', //tells which transpiler to use
+      loader: 'babel-loader', // Tells which transpiler to use
       query: {
-        presets: ['react', 'es2015']  // this could also be put in a .babelrc file
+        presets: ['react', 'es2015']  // This could also be put in a .babelrc file
       }
-    ]
+    }
+   ]
   }
 };
