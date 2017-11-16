2d58bd1a8102974c56c5131f1912ca3fbcc169a8
Jonathan Tang <j.tang17@gmail.com>
2017-11-15 19:43:00 -0800

    first webpack.config.js setup - need path names

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
 const webpack = require('webpack');
 
-const BUILD_DIR;
-const APP_DIR;
+const BUILD_DIR; //path //dirname/src/public bundle.js directory
+const APP_DIR; // index.jsx directory (react file)
+
+module.exports = {
+  entry: APP_DIR + '/index.jsx', //FILE WE WILL BUNDLE FROM
+  output: {
+    path: BUILD_DIR, //WHERE BUNDLE.JS WILL GO
+    filename: 'bundle.js' //TRANSPILED FILE
+  },
+  module: {
+    loaders: [
+      test: /\.jsx?/, //specifying file type that will be transpiled
+      include: APP_DIR,
+      loader: 'babel-loader', //tells which transpiler to use
+      query: {
+        presets: ['react', 'es2015']  // this could also be put in a .babelrc file
+      }
+    ]
+  }
+};
