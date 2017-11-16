ceeb71dd5a9f6535589fdc1b497b2fe9ae71d3de
dwrz <dwrz@dwrz.net>
2017-11-16 22:37:06 +0000

    Import changes.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World.');
+	res.send('Hello, World.');
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+	console.log(`Listening on ${PORT}`);
 });
