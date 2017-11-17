c465ea623d73786e2a6b97cb18080a849e506676
Alexander Mire <amire51117@gmail.com>
2017-11-17 17:18:57 -0500

    dropdown skeleton

--- client/src/DataLogger.jsx
+++ client/src/DataLogger.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Select from 'react-select';
 
 class DataLogger extends React.Component {
 	constructor(props) {
 		super(props);
 		// this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {
+			eventList: [{label: 'butterfly'}, {label: 'coke'}, {label: 'pegions'}],
+			currentEvent: 'No events Selected'
+		}
+		this.logChange = this.logChange.bind(this)
 	}
 
+	logChange(e) {
+		this.setState({currentEvent: 'Current Event ' + e.label});
+	} 
 	// handleSubmit(e) {
 	// 	e.preventDefault();
 	// 	var dataLog = document.forms.logInput;
@@ -14,18 +23,23 @@ class DataLogger extends React.Component {
 	// 	onClick = {this.handleSubmit}
 	// }
 
-
 	render() {
 		return(
-			<div>			
+			<div>
+				<Select
+				name="form-field-name"
+				label='butterfly'
+				options={this.state.eventList}
+				onChange={this.logChange}
+				/>
+				<p>{this.state.currentEvent}</p>
+				<hr />			
 				<form name="logInput">
   				<input type="number" name="units" />
   				<button>Enter unit for Event</button>
   			</form>
   		</div>
-			)
+  		)
 	}
-
-
 }
-module.exports = DataLogger
+export default DataLogger;
