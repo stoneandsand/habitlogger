d9537aafcf2abc2fcb9a26029dce492704cf16ec
Alexander Mire <amire51117@gmail.com>
2017-11-17 15:13:46 -0500

    index.jsx change and DataLogger addition

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from './Table.jsx';
 import Chart from './Chart.jsx';
+import DataLogger from './DataLogger.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // this.logItem = this.logItem.bind(this);
   }
+
+  // logItem(entry, habit) {
+  //   axios.post('/api/' + {this.props.username} + '/log', {
+  //     timestamp: new Date() , habit:'placeholder', unit:entry
+  //   })
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });   
+  // }
 
   render() {
     return (
