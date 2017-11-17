6dbdfc03195d8d9a45dd8f534d5db90e380a23e6
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-16 17:58:40 -0700

    Set up and test database connection, schema, and save function

---
+++ db/index.js
@@ -0,0 +1,81 @@
+// require mongoose in order to use it
+const mongoose = require('mongoose');
+
+// create connection with usersAccounts database
+mongoose.connect('mongodb://localhost/usersAccounts');
+
+// monitor connection
+var database = mongoose.connection;
+
+database.on('error', console.error.bind(console, 'connection error: '));
+database.once('open', function() {
+  console.log('successfully connected to usersAccounts database');
+})
+
+// create schema for a user account
+const userSchema = mongoose.Schema({
+  userName: {type: 'string', unique: true}
+});
+
+// compile userSchema to a model for creating instances
+var User = mongoose.model('User', userSchema);
+
+// create function for server to save a user's info
+module.exports.save = function(object) {
+  // create new instance of schema using passed object
+  object = new User({
+    userName: object.userName
+  })
+
+  // save new User into usersAccounts database
+  object.save((error, object) => {
+    // if there is an error, return in console
+    if(error) {
+      return console.error('error from database save:', error);
+    } else {  // if no errors, give confirmation
+      console.log(object.userName + ' saved to usersAccounts database');
+    }
+  })
+}
+
+// create function for server to retrieve user's info
+module.exports.retrieve = function(query, callback) {
+  // build query
+  User.find(query, (error, user) => {
+    if(error) {
+      return console.error('error from database retrieve: ',error);
+    } else {
+      // send user's info back via callback
+      callback(user);
+      // show confirmation of successful query
+      console.log('this user was queried: ',user)
+    }
+  });
+}
+
+const me = {
+  userName: 'cpbennett4'
+}
+
+module.exports.save(me);
+
+// database = {
+//   {
+//     user: String,
+//     password: String,
+//     habits: {
+//       smoking: {
+//         unit: 'packs',
+//         limit: 5,
+//         timeframe: 'day',
+//         occurrences: [{ timestamp: '20171114', value: 3}]
+//       },
+//       videogames: {
+//         unit: 'hours',
+//         limit: 8,
+//         timeframe: 'week',
+//         occurrences: [{ timestamp: '20171114', value: 3}]
+//       }
+//     }
+//   }
+// };
