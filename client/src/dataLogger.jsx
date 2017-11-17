df5296bb304b0154b37bed756f19a6027ad9c9f0
Alexander Mire <amire51117@gmail.com>
2017-11-16 22:28:15 -0500

    proto dataLogger

---
+++ client/src/dataLogger.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class DataLogger extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.logItem = this.logItem.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		var dataLog = document.forms.logInput;
+
+	}
+
+
+
+	render() {
+		return(
+			<div>			
+				<form name="logInput">
+  				<input type="number" name="units" />
+  				<button onClick = {this.handleSubmit}>Enter unit for Event</button>
+  			</form>
+  		</div>
+			)
+	}
+
+
+}
+module.exports = DataLogger
