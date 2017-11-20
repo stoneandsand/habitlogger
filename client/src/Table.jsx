450fab254a26ccda57223900010006c6dd7fd8d5
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-20 11:57:49 -0800

    Create example data in state and successfully implement row mapping

--- client/src/Table.jsx
+++ client/src/Table.jsx
@@ -9,36 +9,58 @@ import {
   TableRowColumn,
 } from 'material-ui/Table';
 
-const MuiTable = (props) => {
-  return (
-    <div>
-      <h1 className="tableName">Event</h1>
-      <MuiThemeProvider>
-        <Table>
-          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-            <TableRow>
-              <TableHeaderColumn>Time</TableHeaderColumn>
-              <TableHeaderColumn>Quantity</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            <TableRow>
-              <TableRowColumn>Dec 10</TableRowColumn>
-              <TableRowColumn>4 hours</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Dec 11</TableRowColumn>
-              <TableRowColumn>5 hours</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Dec 12</TableRowColumn>
-              <TableRowColumn>6 hours</TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </MuiThemeProvider>
-    </div>
-  )
+class MuiTable extends React.Component {
+  constructor(props) {
+    super(props);
+    // will be using props when connected to server/db
+    // change appropriate lookups
+    this.state = {
+      habit: 'Video Games',
+      limit: 7,
+      unit: 'Hours',
+      timeframe: 'Day',
+      occurrences: [
+        {_id: 0, timestamp: '20 Dec 17', value: '2'},
+        {_id: 1, timestamp: '22 Dec 17', value: '4'},
+        {_id: 2, timestamp: '23 Dec 17', value: '6'},
+        {_id: 3, timestamp: '26 Dec 17', value: '5'},
+        {_id: 4, timestamp: '28 Dec 17', value: '2'},
+        {_id: 5, timestamp: '28 Dec 17', value: '1'}
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="tableName">{this.state.habit}</h1>
+        <h2 className="limitInfo">You set your limit to {this.state.limit} {this.state.unit}/{this.state.timeframe}</h2>
+        <MuiThemeProvider>
+          <Table>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn>Date</TableHeaderColumn>
+                <TableHeaderColumn>{this.state.unit}</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              {
+                this.state.occurrences.map((occurence, index) => {
+                  return (
+                    <TableRow key={occurence._id}>
+                      <TableRowColumn>{occurence.timestamp}</TableRowColumn>
+                      <TableRowColumn>{occurence.value}</TableRowColumn>
+                    </TableRow>
+                  )
+                  counter++;
+                })
+              }
+            </TableBody>
+          </Table>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
 };
 
 export default MuiTable;
