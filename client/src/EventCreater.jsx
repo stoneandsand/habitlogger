57fe78072af1e7a03525f04b7635f43c84972d65
Alexander Mire <amire51117@gmail.com>
2017-11-20 14:44:34 -0500

    added prototype of event creater

---
+++ client/src/EventCreater.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class EventCreater extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      timeFrames: [{ label: 'day', value: 'day' }, { label: 'week', value: 'week' }, { label: 'month', value: 'month' }],
+      selectedTimeFrame : 'day'
+    }
+    this.timeFrameChange = this.timeFrameChange.bind(this);
+  }
+
+  timeFrameChange(e) {
+    this.setState({ currentEvent: `${e.label}` });
+  }
+
+  render() {
+    return (
+      <div>
+        <Select
+          name="form-field-name"
+          label="day"
+          options={this.state.timeFrames}
+          onChange={this.logChange}
+        />
+        <p>Select time frame of data logging</p>
+        <form name="eventCreation">
+          <input type = "text" name="eventName" placeholder="Enter Event name" />
+          <input type = "text" name="units" placeholder="Units used to measure event" />
+          <input type = "number" name="limit" placeholder="Goal/Limit amount of Event" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default EventCreater;
