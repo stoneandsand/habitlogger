6d1ad94ef3906f861841aca6c43f724d2c1bda3f
dwrz <dwrz@dwrz.net>
2017-11-22 14:08:30 +0000

    Refactor schemas into their own file, import schemas in db/index.js

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = require('./schemas.js');
 const DB_URI = process.env.MONGO_URI || 'mongodb://localhost/stoneandsand';
 mongoose.Promise = require('bluebird');
 mongoose.connect(DB_URI);
@@ -10,41 +11,6 @@ database.on('error', console.error.bind(console, 'Connection error: '));
 database.once('open', () => {
   console.log('Successfully connected to database.');
 });
-
-// SCHEMAS
-// A user has many habits, and a habit has many occurrences.
-// A user should only be able to access their own habit.
-// A habit should only ever be able to access its own occurrences.
-// We have decided to use embeded subdocuments to capture this data model.
-// It should simplify querying.
-// However, it may be worth considering using references instead,
-// especially for the users<-->habit relationship.
-
-const occurrenceSchema = new Schema({
-  timestamp: Date,
-  value: Number, // Number of units, e.g., 3 cigars.
-});
-
-const habitSchema = new Schema({
-  // Is unique habit going to be a problem?
-  // If it is a problem, consider using the habitList array.
-  // If the habit exists in the habitList array,
-  // don't allow the user to create a new habit.
-  habit: {type: String, unique: true}, // e.g., smoking.
-  limit: Number,
-  unit: String, // e.g., cigars.
-  timeframe: String, // e.g., day / week / month
-  occurrences: [occurrenceSchema], // Emeded subdocument.
-});
-
-// Schema for a users.
-const userSchema = new Schema({
-  username: {type: String, unique: true},
-  password: String,
-  habitList: Array, // Array of strings, e.g., ['smoking', 'running']
-  habits: [habitSchema],  // Embeded subdocument.
-}); 
-const User = mongoose.model('User', userSchema);
 
 // METHODS
 const signup = (user, cb) => {
