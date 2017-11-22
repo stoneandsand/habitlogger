bd9024cb810b05c74eae42a321b8d8dd526e0486
Jonathan Tang <j.tang17@gmail.com>
2017-11-22 12:56:53 -0500

    event selector operational, logEvent=>logHabit

--- client/src/EventSelector.jsx
+++ client/src/EventSelector.jsx
@@ -6,21 +6,34 @@ class EventSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 1,
+      value: 0,
+      selected: '',
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event, index, value) {
-     this.setState({value: value});
+    this.setState({value});
+    this.setState({
+      selected: this.props.habits[value],
+    });
+    this.props.selectHabit(this.props.habits[value]);
   }
 
   render() {
     return (
-      <SelectField>
-          {this.props.habits.map((habit, index) =>
-            <MenuItem value={index}>{habit}</MenuItem>
-          )}
-      </SelectField>
+      <div>
+        <SelectField
+          floatingLabelText="View Habit Data"
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+            {this.props.habits.map((habit, index) =>
+              <MenuItem value={index}>{habit}</MenuItem>
+            )}
+        </SelectField>
+        <p>Habit: {this.state.selected}</p>
+      </div>
     )
   }
 
