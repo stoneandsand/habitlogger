bd093ce1c96e465ab33c74999016fb8f87524707
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-24 11:09:36 -0800

    Add container to outermost div for app component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -129,7 +129,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <MuiThemeProvider>
           <TopBar />
         </MuiThemeProvider>
