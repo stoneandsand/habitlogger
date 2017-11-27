89986ed37ebdb28013f925d1af436c5ae0ab7540
dwrz <dwrz@dwrz.net>
2017-11-27 19:29:06 +0000

    Fix npm start script for deployment

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./schemas.js');
 const DB_URI = `${process.env.MONGODB_URI}/stoneandsand` || 'mongodb://localhost/stoneandsand';
+
 mongoose.Promise = require('bluebird');
 mongoose.connect(DB_URI);
 
