53647d1bc400fa272ac9ee9590bf01a83f1e23e7
dwrz <dwrz@dwrz.net>
2017-11-27 19:50:02 +0000

    Fix style

--- client/src/Chart.jsx
+++ client/src/Chart.jsx
@@ -13,19 +13,18 @@ class Chart extends React.Component {
           }
         }]
       }
-    }
+    };
 
-
-    this.compileEntryLabels = this.compileEntryLabels.bind(this)
-    this.compileEntryValues = this.compileEntryValues.bind(this)
-    this.setData = this.setData.bind(this)
-    this.sortDates = this.sortDates.bind(this)
+    this.compileEntryLabels = this.compileEntryLabels.bind(this);
+    this.compileEntryValues = this.compileEntryValues.bind(this);
+    this.setData = this.setData.bind(this);
+    this.sortDates = this.sortDates.bind(this);
 
     this.state = {
       labels : this.compileEntryLabels(this.sortDates()),
       data : this.compileEntryValues(this.sortDates()),
       unit : this.props.unit,
-    }
+    };
   }
 
   componentWillMount() {
@@ -36,63 +35,65 @@ class Chart extends React.Component {
     this.setState({
       labels : this.compileEntryLabels(this.sortDates()),
       data : this.compileEntryValues(this.sortDates()),
-    })
+    });
     this.setData(this.state.labels,this.state.data,this.state.unit);
   }
   //uses the sorted and filtered occurrence array that contains timestamp and value
   compileEntryLabels (entries) {
-    entries.length > 40 ? entries = entries.slice(entries.length - 40) : entries;
+    if (entries.length > 40) {
+      entries = entries.slice(entries.length - 40);
+    }
 
     return entries.map((entry) => {
-      return this.props.timeframe + " of " + moment(entry.timestamp).format('MMM Do YYYY')
-    })
+      return this.props.timeframe + " of " + moment(entry.timestamp).format('MMM Do YYYY');
+    });
   }
 
   compileEntryValues (entries) {
-    entries.length > 40 ? entries = entries.slice(entries.length - 40) : entries;
+    entries.length > 40 ? entries = entries.slice(entries.length - 40) : entries;;
 
 
     let arr = entries.map((entry) => {
-      return entry.value
-    })
+      return entry.value;
+    });
     console.log(arr);
     return arr;
   }
 
   sortDates () {
     return this.props.occurrences.sort((a,b)=>{
-      return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
-    })
+      return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
+    });
   }
 
   setData (lab,dat,uni) {
     this.setState(  {data:
-      {
-        labels: lab,
-          datasets: [
-            {
-              label: uni,
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: 'rgba(75,192,192,0.4)',
-              borderColor: 'rgba(75,192,192,1)',
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: 'rgba(75,192,192,1)',
-              pointBackgroundColor: '#fff',
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-              pointHoverBorderColor: 'rgba(220,220,220,1)',
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: dat,
-            }
-          ]
-      } })
+                     {
+                       labels: lab,
+                       datasets: [
+                         {
+                           label: uni,
+                           fill: false,
+                           lineTension: 0.1,
+                           backgroundColor: 'rgba(75,192,192,0.4)',
+                           borderColor: 'rgba(75,192,192,1)',
+                           borderCapStyle: 'butt',
+                           borderDash: [],
+                           borderDashOffset: 0.0,
+                           borderJoinStyle: 'miter',
+                           pointBorderColor: 'rgba(75,192,192,1)',
+                           pointBackgroundColor: '#fff',
+                           pointBorderWidth: 1,
+                           pointHoverRadius: 5,
+                           pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                           pointHoverBorderColor: 'rgba(220,220,220,1)',
+                           pointHoverBorderWidth: 2,
+                           pointRadius: 1,
+                           pointHitRadius: 10,
+                           data: dat,
+                         }
+                       ]
+                     }});
   }
 
   render() {
@@ -101,7 +102,7 @@ class Chart extends React.Component {
         <h3>{this.props.viewHabit} over the past 40 {this.props.timeframe}</h3>
         <Line data={this.state.data} options={this._options}/>
       </div>
-    )
+    );
   }
 }
 
