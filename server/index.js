d3b79e1f0816c4dd2869679edf867e3b7738ab0d
AntonVS <InfiniteStyles.Create@gmail.com>
2017-11-29 20:09:08 -0800

    Touched up the files.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('../db/index.js');
+
+
+
+const PORT = process.env.PORT || 3000;
+
 const session = require('express-session');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
