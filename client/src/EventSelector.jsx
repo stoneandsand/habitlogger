319ca019e25db680581e62f23b6a2d774043d260
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-12-01 20:46:38 +1300

    Fix bug Displayed Habit Data is now updating (#3) (#21)

--- client/src/EventSelector.jsx
+++ client/src/EventSelector.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-class EventSelector extends React.Component {
+export default class EventSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       value: 0,
       currentHabit: '',
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -17,27 +18,24 @@ class EventSelector extends React.Component {
       currentHabit: this.props.habits[index],
       value: index,
     });
+
     this.props.selectHabit(this.props.habits[index]);
   }
 
-  // bug on line 37: displayed data changes when data is logged but displayed habit data does not update
   render() {
     return (
       <div className="viewHabitData">
-      <h1>Chart Selector</h1>
-        <SelectField
-          floatingLabelText="View Habit Data"
-          value={this.state.value}
-          onChange={this.handleChange}
-        >
-            {this.props.habits.map((habit, index) =>
-              <MenuItem key={index} value={index} primaryText={habit} />
-            )}
+        <h1>Chart Selector</h1>
+        <SelectField floatingLabelText="View Habit Data" value={this.state.value} onChange={this.handleChange}>
+          {this.props.habits.map((habit, index) => <MenuItem key={index} value={index} primaryText={habit} />)}
         </SelectField>
-        <p>Displayed Habit Data: {this.props.currentHabit ? this.state.currentHabit : 'None'}</p>
+        <p>Displayed Habit Data: {this.state.currentHabit ? this.state.currentHabit : 'None'}</p>
       </div>
-    )
+    );
   }
 }
 
-export default EventSelector;
+EventSelector.propTypes = {
+  selectHabit: PropTypes.func.isRequired,
+  habits: PropTypes.array.isRequired,
+};
